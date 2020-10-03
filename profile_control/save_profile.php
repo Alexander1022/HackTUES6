05e3b04920aa035539f18813e83562f4e7fa9c29
aon2003 <a.o.naidenov@gmail.com>
2020-10-03 13:56:33 +0300

    Added a register btn in the Login page. Added a countries table.

--- profile_control/save_profile.php
+++ profile_control/save_profile.php
@@ -4,8 +4,6 @@ require_once '../db_connect.php';
 $q = $sql->prepare("INSERT INTO profiles(username, email, password, country)
                     VALUES(:username, :email, :password, :country)");
 
-//die(hash('sha256', $_REQUEST['password']));
-
 $q->execute([
     "username" => $_REQUEST['username'],
     "email" => $_REQUEST["email"],
