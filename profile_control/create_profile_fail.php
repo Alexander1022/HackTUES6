bdbd57ccc98d7d5ac1dedbb989ac21b3b3170f2d
aon2003 <a.o.naidenov@gmail.com>
2020-10-03 17:55:44 +0300

    Changes for Profile controller

---
+++ profile_control/create_profile_fail.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Registration FAILED</title>
+    </head>
+    <body>
+        <?php echo $_COOKIE['err']; $_COOKIE['err'] = NULL ?>
+        Back to <a href="/profile_control/register.html">Registration</a>?
+    </body>
+</html>
--- profile_control/save_profile.php
+++ profile_control/save_profile.php
@@ -4,11 +4,25 @@ require_once '../db_connect.php';
 $q = $sql->prepare("INSERT INTO profiles(username, email, password, country)
                     VALUES(:username, :email, :password, :country)");
 
+if (!$_REQUEST['username']) {
+    $err = "Username not set!";
+}elseif (!$_REQUEST['email']) {
+    $err = "Email not set!";
+}elseif ($_REQUEST["password"] != $_REQUEST['repeat_password']) {
+    $err = "The passwords don't match!";
+}
+
+if (isset($err)) {
+    setcookie('err', $err);
+    header("Location: /profile_control/create_profile_fail.php");
+    exit();
+}
+
 $q->execute([
     "username" => $_REQUEST['username'],
     "email" => $_REQUEST["email"],
     "password" => hash('sha256', $_REQUEST["password"]),
-    "country" => isset($_REQUEST["country"]) ? $_REQUEST["country"] : NULL
+    "country" => isset($_REQUEST["country"]) ? $_REQUEST["country"] : ""
 ]);
 
 header("Location: /profile_control/login.html");
