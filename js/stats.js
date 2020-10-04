76fc8df164cd2aca2923b19e0377fa92a5a522b2
stephie306 <61330933+stephie306@users.noreply.github.com>
2020-10-04 14:46:05 +0300

    stats chenges
    stats changes

--- js/stats.js
+++ js/stats.js
@@ -1,14 +1,17 @@
 var button = document.querySelector('.btn')
 var input = document.querySelector('.in_value')
 
-var ctx = document.getElementById('rightChart');
-var ctx2 = document.getElementById('leftChart');
+//var ctx = document.getElementById('rightChart');
+var ctx = document.getElementById('myChart');
+var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = $('#myChart');
+var ctx = 'myChart';
 
 fetch('https://disease.sh/v3/covid-19/all')
 .then(response => response.json())
 .then(data => {
     document.querySelector(".country").innerHTML = "Global";
-    document.querySelector(".act_cases").innerHTML = "General cases : +" + data['todayCases'];
+    document.querySelector(".act_cases").innerHTML = "New cases : +" + data['todayCases'];
     document.querySelector('.deaths').innerHTML = "Death cases : +" + data['todayDeaths'];
     document.querySelector('.recovered').innerHTML = "Recovered cases : +" + data['todayRecovered'];
 })
@@ -19,91 +22,92 @@ fetch('https://disease.sh/v3/covid-19/all')
         document.querySelector('.recovered').innerHTML = "-";
 })
 
-button.addEventListener('click', function(name){
-    console.log('The location is ' + input.value);
-    fetch('https://disease.sh/v3/covid-19/countries/' + input.value)
-    .then(response => response.json())
-    .then(data => {
-        document.querySelector(".country").innerHTML = data['country'];
-        document.querySelector(".act_cases").innerHTML = "General cases : +" + data['todayCases'];
-        document.querySelector('.deaths').innerHTML = "Death cases : +" + data['todayDeaths'];
-        document.querySelector('.recovered').innerHTML = "Recovered cases : +" + data['todayRecovered'];
-    })
-    
-    .catch(err => {
-        document.querySelector(".country").innerHTML = "-";
-        document.querySelector(".act_cases").innerHTML = "-";
-        document.querySelector('.deaths').innerHTML = "-";
-        document.querySelector('.recovered').innerHTML = "-";
-    })
-    })
-
-fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+fetch('https://disease.sh/v3/covid-19/historical/')
 .then(response => response.json())
-.then(data => {
-    var myChart = new Chart(ctx, {
+.then(data => { 
+    var myLineChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: ['10th June', '10th July', '10th August', '10th September'],
-            datasets: [{
-                label: 'Global COVID-19 Cases in last 120 days',
-                data: [data['cases']['6/10/20'], data['cases']['6/15/20'], data['cases']['7/10/20'], data['cases']['7/15/20'], data['cases']['8/10/20'], data['cases']['8/15/20'], data['cases']['9/10/20'], data['cases'['9/15/20']]],
+            label: 'Graph',
+            datasets: [
+                {
+                label: 'General cases',
+                data: [0, 0],
+                borderColor: 'red',
+				backgroundColor: 'red',
+				fill: false,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(4,139,168, 0.2)',
+                    'rgba(4,139,168, 0.2)',
+                    'rgba(4,139,168, 0.2)',
+                    'rgba(4,139,168, 0.2)',
+                    'rgba(4,139,168, 0.2)',
+                    'rgba(4,139,168, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)'
                 ],
                 borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
+                
+            }, 
+            {
+                label: 'Death cases',
+                data: [0, 0],
+                borderColor: 'blue',
+				backgroundColor: 'blue',
+				fill: false,
+                backgroundColor: [
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)'
+                ],
+                borderWidth: 1
+                 
+            },
+            {
+                label: 'Recovered cases',
+                data: [0, 0],
+                borderColor: 'blue',
+				backgroundColor: 'blue',
+				fill: false,
+                backgroundColor: [
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)'
+                ],
+                borderWidth: 1
+                 
             }
-        }
-    });
-
-    var myChart2 = new Chart(ctx2, {
-        type: 'line',
-        data: {
-            labels: ['10th June', '10th July', '10th August', '10th September'],
-            datasets: [{
-                label: 'Global COVID-19 Cases in last 120 days',
-                data: [data['cases']['6/10/20'], data['cases']['6/15/20'], data['cases']['7/10/20'], data['cases']['7/15/20'], data['cases']['8/10/20'], data['cases']['8/15/20'], data['cases']['9/10/20'], data['cases'['9/15/20']]],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+        ] 
         },
+
         options: {
             scales: {
                 yAxes: [{
@@ -115,4 +119,148 @@ fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
         }
     });
 })
+
+button.addEventListener('click', function(name){
+    console.log('The location is ' + input.value);
+    fetch('https://disease.sh/v3/covid-19/countries/' + input.value)
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector(".country").innerHTML = data['country'];
+        document.querySelector(".act_cases").innerHTML = "New cases : +" + data['todayCases'];
+        document.querySelector('.deaths').innerHTML = "Death cases : +" + data['todayDeaths'];
+        document.querySelector('.recovered').innerHTML = "Recovered cases : +" + data['todayRecovered'];
+    })
+    
+    .catch(err => {
+        document.querySelector(".country").innerHTML = "-";
+        document.querySelector(".act_cases").innerHTML = "-";
+        document.querySelector('.deaths').innerHTML = "-";
+        document.querySelector('.recovered').innerHTML = "-";
+    })
+
+    fetch('https://disease.sh/v3/covid-19/historical/' +  input.value)
+.then(response => response.json())
+.then(data => { 
+    var keys = Object.keys(data['timeline']['cases']);
+    var general_first = keys[0];
+    
+    var general_last = keys[keys.length-1];
+    
+
+    var keys = Object.keys(data['timeline']['deaths']);
+    var death_first = keys[0];
+    var death_last = keys[keys.length-1];
+    
+
+    var keys = Object.keys(data['timeline']['recovered']);
+    var rec_first = keys[0];
+    var rec_last = keys[keys.length-1];
+    
+    //
+    console.log(data['timeline']['cases'][death_first]);
+    console.log(data['timeline']['cases'][general_last]);
+    console.log(data['timeline']['deaths'][death_first]);
+    console.log(data['timeline']['deaths'][death_last]);
+    console.log(data['timeline']['recovered'][rec_first]);
+    console.log(data['timeline']['recovered'][rec_last]);
+
+    var myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            label: 'Graph',
+            datasets: [
+                {
+                label: 'General cases',
+                data: [data['timeline']['cases'][death_first], data['timeline']['cases'][general_last]],
+                borderColor: 'red',
+				backgroundColor: 'red',
+				fill: false,
+                backgroundColor: [
+                    'rgba(4,139,168, 0.2)',
+                    'rgba(4,139,168, 0.2)',
+                    'rgba4,139,168, 0.2)',
+                    'rgba(4,139,168, 0.2)',
+                    'rgba(4,139,168, 0.2)',
+                    'rgba(4,139,168, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)',
+                    'rgba(4,139,168, 1)'
+                ],
+                borderWidth: 1
+                
+            }, 
+            {
+                label: 'Death cases',
+                data: [data['timeline']['deaths'][death_first], data['timeline']['deaths'][death_last]],
+                borderColor: 'blue',
+				backgroundColor: 'blue',
+				fill: false,
+                backgroundColor: [
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)',
+                    'rgba(230,57,70, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(230,57,70, 1)',
+                    'rgba(230,57,70, 1)',
+                    'rgba(230,57,70, 1)',
+                    'rgba(230,57,70, 1)',
+                    'rgba(230,57,70, 1)',
+                    'rgba(230,57,70, 1)'
+                ],
+                borderWidth: 1
+                 
+            },
+            {
+                label: 'Recovered cases',
+                data: [data['timeline']['recovered'][rec_first], data['timeline']['recovered'][rec_last]],
+                borderColor: 'blue',
+				backgroundColor: 'blue',
+				fill: false,
+                backgroundColor: [
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)',
+                    'rgba(42,157,143, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(42,157,143, 1)',
+                    'rgba(42,157,143, 1)',
+                    'rgba(42,157,143, 1)',
+                    'rgba(42,157,143, 1)',
+                    'rgba(42,157,143, 1)',
+                    'rgba(42,157,143, 1)'
+                ],
+                borderWidth: 1
+                 
+            }
+        ] 
+        },
+
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+})
+
+
+    })
+
+
 
