fefcff17e82daadc81b4f1b1cc19dbd83936d5a5
Alexander <36764735+Alexander1022@users.noreply.github.com>
2020-10-04 21:05:11 +0300

    sw.js changes for PWA
    sw.js changes for PWA

--- sw.js
+++ sw.js
@@ -3,7 +3,26 @@ var filesToCache = [
     '/',
     'index.html',
     'css/style.css',
-    'js/script.js'
+    'js/script.js',
+    'db_setup.sql',
+    'db_connect.php',
+    './profile_control/create_profile_fail.php',
+    './profile_control/login.css',
+    './profile_control/login.php',
+    './profile_control/register.css',
+    './profile_control/register.php',
+    './js/Chart.js',
+    './js/chat.js',
+    './js/news_fetching.js',
+    './js/script.js',
+    './js/stats.js',
+    './css/chat.css',
+    './css/navbar.css',
+    './css/news.css',
+    './css/slideshow.css',
+    './css/stats.css',
+    './style.css'
+
   ];
   
   self.addEventListener('install', function(e) {
